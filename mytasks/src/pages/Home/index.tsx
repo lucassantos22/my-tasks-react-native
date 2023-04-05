e3fae1ecb664cfa7c4e1bd30fc02903cdfd3b77a
lucassantos22 <lucas.afsantos@hotmail.com>
2023-04-05 00:54:37 -0300

    chore: add TaskList

--- mytasks/src/pages/Home/index.tsx
+++ mytasks/src/pages/Home/index.tsx
@@ -7,8 +7,9 @@ import {
   StyleSheet,
   SafeAreaView,
   TextInput,
-  FlatList,
 } from 'react-native';
+
+import {TaskList} from '../../components/TaskList';
 
 interface Task {
   id: string;
@@ -44,15 +45,7 @@ export function Home(): JSX.Element {
           <Text style={styles.buttonText}>Adicionar</Text>
         </TouchableOpacity>
         <Text style={styles.titleTasks}>Minhas tarefas</Text>
-        <FlatList
-          data={tasks}
-          keyExtractor={item => item.id}
-          renderItem={({item}) => (
-            <TouchableOpacity style={styles.buttonTask}>
-              <Text style={styles.titleTask}>{item.title}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        <TaskList tasks={tasks} />
       </View>
     </SafeAreaView>
   );
@@ -96,18 +89,6 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#121214',
     fontSize: 18,
-    fontWeight: 'bold',
-  },
-  buttonTask: {
-    backgroundColor: '#29292e',
-    padding: 10,
-    marginTop: 10,
-    borderRadius: 50,
-    alignItems: 'center',
-  },
-  titleTask: {
-    color: '#f1f1f1',
-    fontSize: 20,
     fontWeight: 'bold',
   },
 });
