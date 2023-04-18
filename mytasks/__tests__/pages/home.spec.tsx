e6344e793ad35a680a30ec03c5d2ae97669af563
lucassantos22 <lucas.afsantos@hotmail.com>
2023-04-18 16:12:49 -0300

    chore: add addTask test

--- mytasks/__tests__/pages/home.spec.tsx
+++ mytasks/__tests__/pages/home.spec.tsx
@@ -1,11 +1,27 @@
-import {render} from '@testing-library/react-native';
+import {render, screen, fireEvent} from '@testing-library/react-native';
 import React from 'react';
 import {Home} from '../../src/pages/Home';
+import {TasksContext} from '../../src/context/TasksContext';
 
 describe('Home page', () => {
   it('renders correctly', () => {
-    const app = render(<Home />);
-    const inputNewTask = app.getByPlaceholderText('Nova tarefa...');
+    render(<Home />);
+    const inputNewTask = screen.getByPlaceholderText('Nova tarefa...');
     expect(inputNewTask).toBeDefined();
+  });
+
+  it('addTask should be called if Adicionar button is clicked', () => {
+    const addTask = jest.fn();
+    render(
+      <TasksContext.Provider
+        value={{addTask, tasks: [], removeTask: jest.fn()}}>
+        <Home />
+      </TasksContext.Provider>,
+    );
+    fireEvent.changeText(
+      screen.getByPlaceholderText('Nova tarefa...'),
+      'Tarefa 1',
+    );
+    fireEvent.press(screen.getByText('Adicionar'));
   });
 });
